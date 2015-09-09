0570ed3c8d80f8221f9678bd7ae27a60a2a6d44f
yalei1008 <layliuis@gmail.com>
2015-09-09 11:13:45 -0400

    final version

---
+++ JSFFaceletsTutorial3/src/com/tutorial/LoginBean.java
@@ -0,0 +1,22 @@
+package com.tutorial;
+
+public class LoginBean {
+	    private String name;     
+	private String password;     
+	public String getName ()    
+	{        
+		return name;     
+	}
+    public void setName (final String name)   
+    {         this.name = name;    
+    } 
+    public String getPassword ()  
+    {        
+    	return password;    
+    	} 
+    public void setPassword (final String password)     
+    {       
+    	this.password = password;     
+    	} 
+    } 
+
